e7fe0e51c385fa12048d69dd355496fbc2383f0d
Maciej Reimann <maciej.reimann@gmail.com>
2019-07-28 18:38:19 +0200

    (feature): new project added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Project1 from "./Projects/Project1";
+import Project2 from "./Projects/Project2";
 
 const App = () => (
   <div>
     Webpack with React
     <Project1 />
+    <Project2 />
   </div>
 );
 
