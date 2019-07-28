aa633764a4e1c079d29bcba0b2dcb09f10b76c96
Maciej Reimann <maciej.reimann@gmail.com>
2019-07-28 18:31:57 +0200

    (feature): webpack + react + ts configured
    * staging test
    
    * staging test 2
    
    * Feature/webpack react typescript (#3)
    
    * (feature: Webpack with React): webpack configured
    
    * (feature: Webpack with React): React added
    
    * (feature: wepback setup): react hot reloading
    
    * (feature: Webpack with React): some ts configuration added
    
    * (feature: Webpack with React): Typescript added and it works

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Project1 from "./Projects/Project1";
+
+const App = () => (
+  <div>
+    Webpack with React
+    <Project1 />
+  </div>
+);
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,46 @@
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
   output: {
-    path: __dirname + "/dist",
+    path: path.join(__dirname, "/dist"),
     publicPath: "/",
     filename: "bundle.js"
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin() // ??
+  ],
   devServer: {
     contentBase: "./dist"
   }
