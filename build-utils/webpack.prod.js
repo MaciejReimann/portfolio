5212760c0432a83a261febd53a40afba9c60bbfb
Maciej Reimann <maciej.reimann@gmail.com>
2019-08-24 20:11:36 +0200

    (feature: webpack configuration): webpack dotenv aded

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,13 +1,18 @@
-const { DefinePlugin } = require("webpack");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "production",
   devtool: "source-map",
   plugins: [
-    new DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
+    new Dotenv({
+      path: "./.env.production"
     })
   ]
 };
+
+/*
+ * Now you can introduce sensitive information – such as IP addresses,
+ * account credentials, and API keys/secrets – in your environment variables
+ * via your .env.development and .env.production files. Your Webpack configuration
+ * will copy them over to make them accessible in your source code (see previous section).
+ */
