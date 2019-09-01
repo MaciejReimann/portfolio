a46f1ee6d78330ca3dd5aa75f0df136c3ba6f4c0
Maciej Reimann <maciej.reimann@gmail.com>
2019-09-01 18:33:05 +0200

    feature(import projects as npm modules): tetris imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from "react";
+import Tetris from "@maciejreimann/tetris";
 
 import Project1 from "./Projects/Project1";
 import Project2 from "./Projects/Project2";
 import Project3 from "./Projects/Project3";
 
-const App = () => (
-  <div>
-    Webpack with React
-    <Project1 />
-    <Project2 />
-    <Project3 />
-  </div>
-);
+const App = () => {
+  // const tetris = Tetris();
+  console.log(Tetris.printHello());
+  return (
+    <div>
+      Webpack with React
+      <Project1 />
+      <Project2 />
+      <Project3 />
+    </div>
+  );
+};
 
 export default App;
